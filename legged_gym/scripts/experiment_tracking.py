dcea467d2eff85ee308e59cf37127b7766646d25
Kumaran Akilan <kumaran.akilan@gmail.com>
2023-10-26 13:12:10 -0700

    Update experiment_tracking.py

--- legged_gym/scripts/experiment_tracking.py
+++ legged_gym/scripts/experiment_tracking.py
@@ -1,13 +1,26 @@
+
 import os
 import subprocess
 import mlflow
 import mlflow.pytorch
+
+from legged_gym import LEGGED_GYM_ROOT_DIR, LEGGED_GYM_ENVS_DIR
+
+
 import numpy as np
 
+
+EXPERIMENT_NAME = "MLFlow_Test"
+MAX_ITERATIONS = 1500
+
+loadRange = np.array([0.5, 1, 1.5])
+linkRange = np.array([0.1, 0.2, 0.3])
+
+
 protoType = {
-        "task": "TASK1",
-        "experiment_name": "EXPERIMENT1",
-        "run_name": "RUN1",
+        "task": "a1_flat",
+        "experiment_name": EXPERIMENT_NAME,
+        "run_name": "",
         "seed": 1,
         "actor_hidden_dims": "512,256,128",
         "critic_hidden_dims": "512,256,128",
@@ -27,52 +40,31 @@ protoType = {
         "break_joints_add_observation" : "",
         
         
-        "measure_heights": ""
+        "measure_heights": "True", 
+    
+    
+        "max_iterations" : MAX_ITERATIONS
         
     }
 
-loadRange = np.array([0.5, 1, 1.5])
-linkRange = np.array([0.1, 0.2, 0.3])
-
-
 
 baseMassExperiments = []
-
-
 for baseMass in [True, False]:
     if baseMass:
         for i in loadRange:
-            
-            
             for o in [True, False]:
                 
                 dct = {"randomize_base_mass": baseMass,
                 "randomize_base_mass_range" : str(-1*i)+","+str(i),
-                "randomize_base_mass_add_observation" : str(o)}
-                
-                
+                "randomize_base_mass_add_observation" : o}
                 baseMassExperiments.append(dct)
-                
-                #print(baseMass, -1*i, i, o)
-                #print(dct)
-                
-        
     else:
-        
-        
-        for o in [True, False]:
-            dct = {"randomize_base_mass": baseMass,
+        dct = {"randomize_base_mass": baseMass,
                 "randomize_base_mass_range" : "0,0",
-                "randomize_base_mass_add_observation" : str(o)}
-            
-            #print(baseMass, -1*i, i, o)
-            
-            baseMassExperiments.append(dct)
-            #print(dct)
+                "randomize_base_mass_add_observation" : False}
+        baseMassExperiments.append(dct)
         
     print("\n\n")
-
-
 
 
 linkMassExperiments = []
@@ -81,99 +73,61 @@ linkMassExperiments = []
 for linkMass in [True, False]:
     if linkMass:
         for i in linkRange:
-            
-            
             for o in [True, False]:
-                
                 dct = {"randomize_link_mass": linkMass,
                 "randomize_link_mass_range" : str(1 - i)+","+str(1 + i),
-                "randomize_link_mass_add_observation" : str(o)}
-                
-                
-                linkMassExperiments.append(dct)
-                
-                #print(baseMass, -1*i, i, o)
-                #print(dct)
-                
-        
+                "randomize_link_mass_add_observation" : o}                
+                linkMassExperiments.append(dct)        
     else:
-        
-        
-        for o in [True, False]:
-            dct = {"randomize_link_mass": linkMass,
+        dct = {"randomize_link_mass": linkMass,
                 "randomize_link_mass_range" : "1,1",
-                "randomize_link_mass_add_observation" : str(o)}
-            
-            #print(baseMass, -1*i, i, o)
-            
-            linkMassExperiments.append(dct)
-            #print(dct)
+                "randomize_link_mass_add_observation" : False}
+        linkMassExperiments.append(dct)
         
     print("\n\n")
 
 
-
-
-
-
-
 breakJointsExperiments = []
-
 for break_joints in [True, False]:
     if break_joints:
         for break_joints_add_observation in [True, False]:
-            
-            dct = {"break_joints": break_joints, "break_joints_add_observation": break_joints_add_observation}
-            
+            dct = {"break_joints": break_joints, "break_joints_add_observation": break_joints_add_observation}            
             breakJointsExperiments.append(dct)
-            
-            #break_joints_add_observation
+
     else:
         dct = {"break_joints": break_joints, "break_joints_add_observation": False}
-        
         breakJointsExperiments.append(dct)
+
 
 
 # Define your experiments
 experiments = []
 
-
-for expList in baseMassExperiments + linkMassExperiments + breakJointsExperiments:
-    
+ctr = 0
+for expList in baseMassExperiments + linkMassExperiments + breakJointsExperiments:    
     protoType2 = protoType.copy()
     
     for key in expList:
-        
         protoType2[key] = expList[key]
-        #print(baseExp[key])
-    
-    
-    protoType2["measure_heights"] = False
+
+    protoType2["run_name"] = "Run_"+str(ctr)    
     experiments.append(protoType2)
-    
-    protoType3 = protoType2.copy()
-    protoType3["measure_heights"] = True
-    experiments.append(protoType3)
 
-
-
-
-
+    ctr+=1
 
 
 # Define the path to your train.py script
 train_script_path = "train.py"
+master_log_dir = f'{LEGGED_GYM_ROOT_DIR}'
 
 # Define the base directory where train.py saves the logs and models
-base_log_dir = "/"
+base_log_dir = os.path.join(master_log_dir, "legged_gym", "logs")
+mlflow_log_dir = os.path.join(master_log_dir, "legged_gym", "mlFlowLogs", EXPERIMENT_NAME +"/" )
 
 
 
 
-
-
-
-
+mlflow.set_tracking_uri(mlflow_log_dir)
 # Run each experiment
 for exp in experiments:
     with mlflow.start_run():
@@ -185,8 +139,10 @@ for exp in experiments:
         # Build the command to run train.py
         command = ["python", train_script_path]
         for param_name, param_value in exp.items():
-            command.append(f"--{param_name}")
-            command.append(str(param_value))
+            if param_value != False and param_value != '':
+                command.append(f"--{param_name}")
+                if param_value!= True:
+                    command.append(str(param_value))
         
         # Run train.py
         subprocess.run(command)
@@ -197,8 +153,13 @@ for exp in experiments:
         log_dir = os.path.join(base_log_dir, experiment_name, run_name)
         
         
-        model_path = os.path.join(log_dir, "model_<iteration>.pt")  # modify as needed
-        tensorboard_log_dir = os.path.join(log_dir, "tensorboard_logs")  # modify as needed
-        
+        model_path = os.path.join(log_dir, "model_"+str(MAX_ITERATIONS)+".pt")  # modify as needed
         mlflow.pytorch.log_model(model_path, "model")
-        mlflow.log_artifacts(tensorboard_log_dir, "tensorboard_logs")
+        
+        
+        mlflow.log_artifacts(log_dir, "tensorboard_logs")
+
+
+
+
+
